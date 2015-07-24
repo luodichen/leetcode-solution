c05c0af8db796c17670981f28e5ae01c75980533
luodichen <me@luodichen.com>
2015-07-24 11:22:42 +0800

    C solution with 136

---
+++ accepted/136-single-number.c
@@ -0,0 +1,7 @@
+/* https://leetcode.com/problems/single-number/ */
+int singleNumber(int* nums, int numsSize) {
+    int ret = 0;
+    for (int i = 0; i < numsSize; i++)
+        ret ^= nums[i];
+    return ret;
+}
