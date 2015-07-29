88acc1b33a60de48a573dee7db61b6f460d7201c
luodichen <me@luodichen.com>
2015-07-29 18:50:43 +0800

    55

--- accepted/55-jpmp-game.c
+++ accepted/55-jpmp-game.c
@@ -3,10 +3,8 @@ bool canJump(int* nums, int numsSize)
 {
     int nFarthest = 0;
     for (int i = 0; (i < numsSize && i <= nFarthest); i++)
-    {
         if (i + nums[i] > nFarthest)
             nFarthest = i + nums[i];
-    }
     
     return nFarthest >= numsSize - 1;
 }
