bcdf01ce97181531ccab41ca8b58a4b41e75f639
luodichen <me@luodichen.com>
2015-08-13 14:51:03 +0800

    227 accepted

---
+++ accepted/227-basic-calculator-ii.py
@@ -0,0 +1,63 @@
+# https://leetcode.com/problems/basic-calculator-ii/
+class Solution:
+    digits = set('0123456789')
+    symbolset = set('+-*/()')
+    
+    def __init__(self):
+        self.init()
+    
+    def init(self):
+        self.numbers = []
+        self.symbols = []
+        self.num_read = False
+        self.num = 0
+        
+    def number_got(self, priority):
+        while len(self.symbols) > 0 and self.symbols[len(self.symbols) - 1] != '(':
+            if priority > 0 and self.symbols[len(self.symbols) - 1] in set('+-'):
+                break
+            
+            right = self.numbers.pop()
+            left = self.numbers.pop()
+            symbol = self.symbols.pop()
+            
+            if symbol == '+':
+                result = left + right
+            elif symbol == '-':
+                result = left - right
+            elif symbol == '*':
+                result = left * right
+            elif symbol == '/':
+                result = left / right
+                
+            self.numbers.append(result)
+        
+    def input(self, ch):
+        if ch in self.digits:
+            self.num_read = True
+            self.num = self.num * 10 + int(ch)
+        elif ch in self.symbolset:
+            if self.num_read:
+                self.numbers.append(self.num)
+                self.num = 0
+                self.number_got({'(': 0, ')': 0, '+': 0, '-': 0, '*': 1, '/': 1, }[ch])
+                
+            self.num_read = False
+            self.symbols.append(ch)
+        
+        if ch == ')':
+            self.symbols.pop()
+            self.symbols.pop()
+            self.number_got(0)
+        
+    # @param {string} s
+    # @return {integer}
+    def calculate(self, s):
+        self.init()
+        for ch in s:
+            self.input(ch)
+        
+        self.numbers.append(self.num)
+        self.number_got(0)
+        
+        return self.numbers[0]
