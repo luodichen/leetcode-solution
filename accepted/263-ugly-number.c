7926d3c2e4bcb21f75b4ebad60fe2caef4226b2f
luodichen <me@luodichen.com>
2015-08-19 15:43:34 +0800

    remove useless code

--- accepted/263-ugly-number.c
+++ accepted/263-ugly-number.c
@@ -2,8 +2,6 @@
 bool isUgly(int num) 
 {
     int nPrimes[] = {2, 3, 5};
-    if (num < 1)
-        return false;
     
     for (int i = 0; i < (sizeof(nPrimes) / sizeof(nPrimes[0])); i++)
         while ((num > 0) && (num % nPrimes[i] == 0))
